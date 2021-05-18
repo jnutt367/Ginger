a1f4e0ef4ee36d89a5b8cd05df82001cba4dd068
Jason Nutt <jnutt367@gmail.com>
2021-05-18 18:53:37 -0400

    commit last ginger title caps

--- gsap.js
+++ gsap.js
@@ -1,4 +1,4 @@
 gsap.to("#ginger", { fill: "orange" })
 gsap.to("#ginger", { x: 10, y: -10, scale: 4.2, fill: "black", ease: "elastic" }, 2)
 gsap.to("body", { duration: 2, backgroundColor: "white" })
-gsap.fromTo("#text", { x: -800, y: 1800 }, { fill: "orange", fontFamily: "fantasy", duration: 3, scale: 8, x: 200, y: 1600 }, 20)
+gsap.fromTo("#text", { x: -800, y: 1800 }, { fill: "orange", fontFamily: "fantasy", duration: 3, scale: 8.5, x: 200, y: 1600 }, 20)
